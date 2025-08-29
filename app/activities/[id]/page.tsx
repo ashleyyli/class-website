9bddd1f72e863f4c8d495168edba2b48f8104e89
Ashley Li <ashleyl7@illinois.edu>
2025-08-29 17:44:20 -0500

    does this work

--- app/activities/[id]/page.tsx
+++ app/activities/[id]/page.tsx
@@ -5,10 +5,10 @@ import { ActivityContent } from "../../ui/content";
 /**
  * Shows when the /new route is intercepted
  */
-export default async function ActivityModal(props: { params: { id: string } }) {
+export default async function ActivityModal({params}: {params: Promise<{ id: string }>}) {
   console.log("I am the modal that is shown during interception");
-  const id = Number(props.params.id);
-  const activity: Activity | null = await FetchActivitybyId(id);
+  const { id } = await params;
+  const activity: Activity | null = await FetchActivitybyId(Number(id));
 
   if (!activity) {
     return (
